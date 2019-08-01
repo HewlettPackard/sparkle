015a2b0e7c3ac79c6bb0cc4a75419d0cad8abb4f
Debdipta Ghosh <debdipta1078@gmail.com>
2019-08-01 12:21:45 +0530

    Start Multiple Worker from start-spark.sh

--- deploy/virtualnode/templates/scripts/start-spark.sh
+++ deploy/virtualnode/templates/scripts/start-spark.sh
@@ -4,11 +4,15 @@ echo "start master...."
 
 sleep 10s 
 
+export SPARK_WORKER_WEBUI_PORT=8081
+export SPARK_WORKER_INSTANCES=1
+
 {% for i in range(0, num_workers) %}
 echo "start worker{{i}}...."
 export SPARK_IDENT_STRING=worker{{i}}
 ./multicore/worker{{i}}/sbin/start-slave.sh spark://{{ansible_hostname}}:7077
 
+((SPARK_WORKER_WEBUI_PORT++))
+((SPARK_WORKER_INSTANCES++))
+
 {% endfor %}
-
-
